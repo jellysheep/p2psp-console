5986cfc697f7928911304c1306d001bcc298674c
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2016-04-12 22:12:39 +0200

    Improving make.py

--- make.py
+++ make.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import os
 import sys
 import shutil
@@ -8,12 +9,19 @@ import platform
 bin_dir = 'bin'
 build_dir = 'build'
 only_cmake = False
+cmake = 'cmake'
 
 if len(sys.argv) >= 2:
+    if sys.argv[1] == '-h':
+        print("{clean|debug}")
+        quit()
     if sys.argv[1] == 'clean':
         os.system('git clean -dfx')
         quit()
+    if sys.argv[1] == 'debug':
+        cmake = cmake + ' -DCMAKE_BUILD_TYPE=Debug'
     elif sys.argv[1] == 'only-cmake':
+#        cmake = cmake + ' -DTRACE_SILENT_MODE'
         only_cmake = True
 
 if not os.path.exists(bin_dir):
@@ -32,13 +40,12 @@ if not os.path.exists(build_dir):
 sys_name = platform.system()
 
 if sys_name == 'Linux' or sys_name == 'Darwin':
-    print '\nMaking for Linux...\n'
-    print 'Building P2PSP Library...\n'
-    if os.system('cd lib/p2psp/ && ./make.py && echo') == 0:
-        print 'Building console interface...\n'
-        if os.system('cd build && cmake .. && echo') == 0:
-            if not only_cmake:
-                os.system('cd build && make')
+    print('\nMaking for Linux...\n')
+    command = 'cd build && ' + cmake + ' .. && echo'
+    if os.system(command) == 0:
+        command = 'cd build && make'
+        if not only_cmake:
+            os.system(command)
 
 elif sys_name == 'Windows':
-    print '\nMaking for Windows...\n'
+    print('\nMaking for Windows...\n')
