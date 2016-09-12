d23303e34f815aa2db66140b9c63c7166f79368b
kshi219 <kvnshi219@gmail.com>
2016-09-11 23:20:26 -0700

    updated test case 1

--- src/EMS_Tests/EMS_test1.py
+++ src/EMS_Tests/EMS_test1.py
@@ -6,143 +6,61 @@ import sys
 #The Test Rationale is simple, check if all members of the team are sending/receiving chunks as they should
 # from every other member of the team, if yes than this mean the team is up and running correctly
 
-
+print "Running EMS test for both peers behind the same NAT router \n"
 # Namespace addresses and ports of the team stored as a dictionary
 addresses = dict(pc1="192.168.56.4", pc2="192.168.56.5", nat1="192.168.56.6", splitter="192.168.57.6",
                  monitor="192.168.57.7", nat1_pub="192.168.57.4", p2Port="41666", p1Port="41667", monitorPort="4555",
                  splitterPort="4554")
 
-# check if splitter has right outputs, that it is sending chunks to monitor and both peers
+# check if splitter has right outputs, that it has all correct members of the team
 
 splitterFunctional = False
 with open("splitter.log") as f:
-    monitor = False
-    peer1 = False
-    peer2 = False
     for line in f:
-        if  monitor and peer1 and peer2:
+        if re.match(r'.+%(monitor)s.+%(nat1_pub)s.+%(nat1_pub)s.+' % addresses, line, re.M | re.I):
+            print "splitter has correct peers list"
             splitterFunctional = True
             break
-        if not monitor :
-            if "-> %(monitor)s:%(monitorPort)s" % addresses in line:
-                print "splitter sending chunks to monitor"
-                monitor = True
-        if not peer1 :
-            if "-> %(nat1_pub)s:%(p1Port)s" % addresses in line:
-                print "splitter sending chunks to peer1"
-                peer1 = True
-        if not peer2 :
-            if "-> %(nat1_pub)s:%(p2Port)s" % addresses in line:
-                print "splitter sending chunks to peer2"
-                peer2 = True
 
-    if not monitor:
-        print "splitter not sending chunks to monitor"
-    if not peer1:
-        print "splitter not sending chunks to peer1"
-    if not peer2:
-        print "splitter not sending chunks to peer2"
-
+    if not splitterFunctional: print "splitter does not have correct peers list"
 
 print "\n"
 
-# check if monitor has right outputs, that it is recieving chunks from splitter and both peers
+# check if monitor has right outputs, that it has all correct members of the team
 monitorFunctional = False
 with open("monitor.log") as f:
-    splitter = False
-    peer1 = False
-    peer2 = False
     for line in f:
-        if  splitter and peer1 and peer2:
+        if re.match(r'.+%(nat1_pub)s.+%(nat1_pub)s.+' % addresses, line, re.M | re.I):
+            print "monitor has correct peers list"
             monitorFunctional = True
             break
-        if not splitter :
-            if re.match( r'.+\(0.0.0.0,4555\)<-\d+-\(%(splitter)s,%(splitterPort)s\).+' %addresses, line, re.M|re.I):
-                print "monitor receiving chunks from splitter"
-                splitter = True
-        if not peer1 :
-            if re.match( r'.+\(0.0.0.0,4555\)<-\d+-\(%(nat1_pub)s,%(p1Port)s\).+' %addresses, line, re.M|re.I):
-                print "monitor receiving chunks from peer1"
-                peer1 = True
-        if not peer2 :
-            if re.match( r'.+\(0.0.0.0,4555\)<-\d+-\(%(nat1_pub)s,%(p2Port)s\).+'%addresses, line, re.M|re.I):
-                print "monitor receiving chunks from peer2"
-                peer2 = True
 
-    if not splitter:
-        print "monitor not receiving chunks from splitter"
-    if not peer1:
-        print "monitor not receiving chunks from peer1"
-    if not peer2:
-        print "monitor not receiving chunks from peer2"
+    if not monitorFunctional: print "monitor does not have correct peers list"
 
 print "\n"
 
-# check if peer1 has right outputs, that it is recieving chunks from splitter and both peer2 + monitor
+# check if peer1 has right outputs, that it has all correct members of the team
 peer1Functional = False
 with open("peer1.log") as f:
-    splitter = False
-    monitor = False
-    peer2 = False
     for line in f:
-        if  splitter and monitor and peer2:
+        if re.match(r'.+%(monitor)s.+%(pc2)s.+' % addresses, line, re.M | re.I):
+            print "peer1 has correct peers list"
             peer1Functional = True
             break
-        if not splitter :
-            if re.match( r'.+\(0.0.0.0,41667\)<-\d+-\(%(splitter)s,%(splitterPort)s\).+' % addresses
-                    , line, re.M|re.I):
-                print "peer1 receiving chunks from splitter"
-                splitter = True
-        if not monitor :
-            if re.match( r'.+\(0.0.0.0,41667\)<-\d+-\(%(monitor)s,%(monitorPort)s\).+' % addresses
-                    , line, re.M|re.I):
-                print "peer1 receiving chunks from monitor"
-                monitor = True
-        if not peer2 :
-            if re.match( r'.+\(0.0.0.0,41667\)<-\d+-\(%(pc2)s,%(p2Port)s\).+' % addresses
-                    , line, re.M|re.I):
-                print "peer1 receiving chunks from peer2"
-                peer2 = True
 
-    if not splitter:
-        print "peer1 not receiving chunks from splitter"
-    if not monitor:
-        print "peer1 not receiving chunks from monitor"
-    if not peer2:
-        print "peer1 not receiving chunks from peer2"
+    if not peer1Functional: print "peer1 does not have correct peers list"
 
 print "\n"
-
-# check if peer2 has right outputs, that it is recieving chunks from splitter and both peer1 + monitor
+# check if peer2 has right outputs, that it has all correct members of the team
 peer2Functional = False
 with open("peer2.log") as f:
-    splitter = False
-    monitor = False
-    peer1 = False
     for line in f:
-        if  splitter and monitor and peer1:
+        if re.match(r'.+%(monitor)s.+%(pc1)s.+' % addresses, line, re.M | re.I):
+            print "peer2 has correct peers list"
             peer2Functional = True
             break
-        if not splitter :
-            if re.match( r'.+\(0.0.0.0,41666\)<-\d+-\(%(splitter)s,%(splitterPort)s\).+' %addresses, line, re.M|re.I):
-                print "peer2 receiving chunks from splitter"
-                splitter = True
-        if not monitor :
-            if re.match( r'.+\(0.0.0.0,41666\)<-\d+-\(%(monitor)s,%(monitorPort)s\).+'%addresses, line, re.M|re.I):
-                print "peer2 receiving chunks from monitor"
-                monitor = True
-        if not peer1 :
-            if re.match( r'.+\(0.0.0.0,41666\)<-\d+-\(%(pc1)s,%(p1Port)s\).+'%addresses, line, re.M|re.I):
-                print "peer2 receiving chunks from peer1"
-                peer1 = True
 
-    if not splitter:
-        print "peer2 not receiving chunks from splitter"
-    if not monitor:
-        print "peer2 not receiving chunks from monitor"
-    if not peer1:
-        print "peer2 not receiving chunks from peer1"
-
+    if not peer2Functional: print "peer2 does not have correct peers list"
 print "\n"
 
 
